aa4f35b70c8ed54b5268e39c8d62ccdef8cc4efe
Hafiz-AR <hafiz.ar03@yahoo.co.id>
2026-02-06 22:24:17 +0700

    First fully functional version

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,113 @@
 var toDoList = []
 
 function addToDo() {
-    const toDoText = document.getElementById('add-text')
-    const toDoDate = document.getElementById('add-date')
+    const addText = document.getElementById('add-text')
+    const addDate = document.getElementById('add-date')
     
-    if (toDoText.value === '' || toDoDate.value === ''){
+    if (addText.value === '' || addDate.value === ''){
         alert('Activity and Date must be filled')
     }
     else {
         var newItem = {
-            text: toDoText.value,
-            date: toDoDate.value
+            text: addText.value,
+            date: addDate.value
         };
         toDoList.push(newItem)
-        // console.log(toDoList[1].date)
-        renderToDos()
+        addText.value = ''
+        addDate.value = ''
+        renderList(toDoList)
     }
 }
 
-function renderToDos() {
-    const toDoItemList = document.getElementById('item-list')
-    console.log(toDoItemList)
+function renderList(list) {
+    var toDoTable = document.getElementById('to-do-table')
+    toDoTable.innerHTML = `            
+        <tr>
+            <th class="text-default" width="80%">Activity</th>
+            <th class="text-default" width="20%">Date</th>
+        </tr>`
+    list.forEach(
+        addItem = (item) => {
+            toDoTable.innerHTML +=`
+        <tr>
+            <td class="text-thin">${item.text}</td>
+            <td class="text-thin">${item.date}</td>
+        </tr>`
+        }
+    )
+}
+
+function deleteToDo() {
+    if (confirm('Delete all activity from list?')){
+        var toDoTable = document.getElementById('to-do-table')
+        toDoList = []
+        toDoTable.innerHTML = `            
+            <tr>
+                <th class="text-default" width="80%">Activity</th>
+                <th class="text-default" width="20%">Date</th>
+            </tr>        
+            <tr>
+            <td class="text-thin">-</td>
+            <td class="text-thin">-</td>
+        </tr>`   
+    }
+}
+
+function filterToDo() {
+    var filterTable = []
+    var match = false
+    const filterText = document.getElementById('filter-text')
+    const filterDate = document.getElementById('filter-date')
+    console.log(filterText.value)
+    console.log(filterDate.value)
+    
+    if (!(filterText.value === '' && filterDate.value === '')){
+        toDoList.forEach(
+            checkFilter = (item) => {
+                console.log(item.text)
+                if (!(filterText.value === '' || filterDate.value === '')){
+                    if (String(item.text).includes(filterText.value) && filterDate.value === item.date){
+                        match = true
+                    }
+                }
+                else {
+                    if (!(filterText.value === '') && String(item.text).includes(filterText.value)) {
+                        match = true
+                    }
+                    if (!(filterDate.value === '') && filterDate.value === item.date) {
+                        match = true
+                    }
+                }
+                if (match) {
+                    filterTable.push(item)
+                    console.log('here')
+                }
+                match = false
+            }
+        )
+        if (filterTable.length == 0) {
+            filterTable = [{
+                text: '-',
+                date: '-'
+            }]
+        }
+        renderList(filterTable)
+        const filterForm = document.getElementById('misc-buttons')
+        filterForm.innerHTML = `
+            <button id="filter-button" onclick="filterToDo()">Apply Filter</button>
+            <button id="reset-button" onclick="resetFilter()">Reset Filter</button>
+            <button id="delete-button" onclick="deleteToDo()">Clear List</button>`
+    }
+}
+
+function resetFilter() {
+    renderList(toDoList)
+    const filterText = document.getElementById('filter-text')
+    const filterDate = document.getElementById('filter-date')
+    filterText.value = ''
+    filterDate.value = ''
+    const filterForm = document.getElementById('misc-buttons')
+    filterForm.innerHTML = `
+        <button id="filter-button" onclick="filterToDo()">Apply Filter</button>
+        <button id="delete-button" onclick="deleteToDo()">Clear List</button>`
 }
